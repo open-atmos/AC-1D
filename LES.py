7340d6c78a9abe977ede2725c8b9e597578d2ce5
Israel Silber <ixs34@psu.edu>
2022-09-13 16:48:28 -0400

    Correct scaling for ql in bulk mcphys

--- LES.py
+++ LES.py
@@ -286,14 +286,15 @@ class DHARMA(LES):
             If False, using bulk microphysics output namelist for harvesting LES data.
         """
         super().__init__(q_liq_pbl_cut=q_liq_pbl_cut)
-        if les_bin_phys: 
+        if les_bin_phys:
             self.Ni_field = {"name": "ntot_3", "scaling": 1e6}  # scale to m^-3
             self.pflux_field = {"name": "pflux_3", "scaling": 1}  # scale to mm/h
+            self.q_liq_field = {"name": "ql", "scaling": 1e-3}  # scale to kg/kg
         else:  # bulk
             self.Ni_field = {"name": ["nqic", "nqid", "nqif"], "scaling": 1e6}  # scale to m^-3
             self.pflux_field = {"name": ["PFqic", "PFqid", "PFqif"], "scaling": 1}  # scale to mm/h
+            self.q_liq_field = {"name": "ql", "scaling": 1}  # scale to kg/kg
         self.T_field = {"name": "T", "addition": 0}  # scale to K (addition)
-        self.q_liq_field = {"name": "ql", "scaling": 1e-3}  # scale to kg/kg
         self.RH_field = {"name": "RH", "scaling": 1. / 100.}  # scale to fraction
         self.model_name = "DHARMA"
         self.time_dim = "time"
